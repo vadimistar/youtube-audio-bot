74ae5e1adbdfd5e0d5605c9c730247ae8c6e10e6
vadimistar <vadimistar@gmail.com>
2023-09-15 10:48:01 +0300

    Telegram bot responds with the received message

--- cmd/bot/main.go
+++ cmd/bot/main.go
@@ -1,7 +1,24 @@
 package main
 
-import "fmt"
+import (
+	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
+	"github.com/joho/godotenv"
+	"github.com/vadimistar/youtube-audio-bot/internal/telegram"
+	"log"
+	"net/http"
+	"os"
+)
 
 func main() {
-	fmt.Println("Hello, World!")
+	godotenv.Load()
+
+	bot, err := tgbotapi.NewBotAPI(os.Getenv("TELEGRAM_TOKEN"))
+	if err != nil {
+		log.Fatalf("new telegram bot API: %s", err)
+	}
+
+	telegramBot := telegram.NewBot(bot)
+	mux := http.NewServeMux()
+	mux.HandleFunc("/", telegramBot.WebhookHandler)
+	log.Fatal(http.ListenAndServe(":"+os.Getenv("PORT"), mux))
 }
--- go.mod
+++ go.mod
@@ -1,3 +1,7 @@
 module github.com/vadimistar/youtube-audio-bot
 
 go 1.20
+
+require github.com/go-telegram-bot-api/telegram-bot-api/v5 v5.5.1
+
+require github.com/joho/godotenv v1.5.1 // indirect
