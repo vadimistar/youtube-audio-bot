75665cf47c0ee966770350173ac87bbe18818b88
vadimistar <vadimistar@gmail.com>
2023-09-15 23:07:49 +0300

    Task entities for worker container

---
+++ internal/entity/task.go
@@ -0,0 +1,11 @@
+package entity
+
+type TaskRequest struct {
+	ChatID   int64  `json:"chat_id"`
+	VideoURL string `json:"video_url"`
+}
+
+type TaskResponse struct {
+	ChatID       int64  `json:"chat_id"`
+	FileLocation string `json:"file_location"`
+}
